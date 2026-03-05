cff78c6abad74874f240bfc024c2c89b4e8d7897
preetshah029 <preetshah029@gmail.com>
2026-03-05 09:25:33 +0530

    Refactor code structure for improved readability and maintainability

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str as SupportStr;
+use Pest\Support\Str;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'featured_image',
+        'excerpt',
+        'content',
+        'status',
+    ];
+    public function casts()
+    {
+        return [
+            'published_at' => 'datetime',
+        ];
+
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($post) {
+            if (empty($post->slug)) {
+            $post->slug = SupportStr::slug($post->title);
+            }
+        });
+    }
 }
